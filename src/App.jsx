d8a70229bbca0ff763b251df40d7d6b75494aadb
Nelson Rivas <nelson.rivas.ucv+code@gmail.com>
2022-08-11 09:00:21 -0400

    adding tailwind framework

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,11 @@
 import './App.css';
-import { useState } from 'react';
-import MyComponent from './components/MyComponent';
 
 function App() {
-  return (
-    <div className='App'>
-      <h1>Hello World</h1>
-      <MyComponent
-        pr1={'o'}
-        pr2={'6'}
-      />
-    </div>
-  );
+	return (
+		<div className='text-3xl font-bold text-purple-600 underline'>
+			<h1>Hello World</h1>
+		</div>
+	);
 }
 
 export default App;
